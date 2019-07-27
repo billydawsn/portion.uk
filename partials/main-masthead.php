7f6d1d45d75f6ce2ed31fc780882b8e2565c5210
Beanies <beanies@Beaniess-iMac.local>
2019-07-27 13:33:53 +0100

    Home Masthead Changes

--- partials/main-masthead.php
+++ partials/main-masthead.php
@@ -2,7 +2,7 @@
   <div class="container narrow">
     <div class="copy-block" data-jarallax-element="-30">
       <img class="logo" src="/wp-content/themes/portion.uk/assets/images/portion-logo-orange.png">
-      <h2># Give a portion of your Amazon order to charity with just one click.</h2>
+      <h2>Give a portion of your Amazon order to charity with just one click.</h2>
       <a href="/"><button>Shop on Amazon<img src="/wp-content/themes/portion.uk/assets/images/arrow.png"></button></a>
     </div>
     <div class="header-scroll-icon"></div>
