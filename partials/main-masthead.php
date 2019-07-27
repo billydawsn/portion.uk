a906ae36ad442e6fc72c13e6a729d4cf4b8d0c6f
Beanies <beanies@Beaniess-iMac.local>
2019-07-27 13:29:18 +0100

    Home Masthead Changes

--- partials/main-masthead.php
+++ partials/main-masthead.php
@@ -1,9 +1,9 @@
-<div class="row masthead full-masthead main-masthead" style="overflow-x: hidden;background: linear-gradient(to right, rgba(256, 256, 256, 0.7), rgba(256, 256, 256, 0.1)), url('/wp-content/themes/portion/assets/images/confetti.png'); background-size: cover; height: calc(100vh - 4em);">
+<div class="row masthead full-masthead main-masthead" style="overflow-x: hidden;background: linear-gradient(to right, rgba(256, 256, 256, 0.7), rgba(256, 256, 256, 0.1)), url('/wp-content/themes/portion.uk/assets/images/confetti.png'); background-size: cover; height: calc(100vh - 4em);">
   <div class="container narrow">
     <div class="copy-block" data-jarallax-element="-30">
-      <img class="logo" src="/wp-content/themes/portion/assets/images/portion-logo-orange.png">
+      <img class="logo" src="/wp-content/themes/portion.uk/assets/images/portion.uk-logo-orange.png">
       <h2>Give a portion of your Amazon order value to charity with just one click.</h2>
-      <a href="/"><button>Shop on Amazon<img src="/wp-content/themes/portion/assets/images/arrow.png"></button></a>
+      <a href="/"><button>Shop on Amazon<img src="/wp-content/themes/portion.uk/assets/images/arrow.png"></button></a>
     </div>
     <div class="header-scroll-icon"></div>
   </div>
