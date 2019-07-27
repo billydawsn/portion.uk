6c50724b6ff765dafeae465b73deae123626cfdc
Beanies <beanies@Beaniess-iMac.local>
2019-07-27 14:24:11 +0100

    Masthead Changes

--- partials/main-masthead.php
+++ partials/main-masthead.php
@@ -1,4 +1,4 @@
-<div class="row masthead full-masthead main-masthead" style="overflow-x: hidden;background: linear-gradient(to right, rgba(256, 256, 256, 0.7), rgba(256, 256, 256, 0.1)), url('/wp-content/themes/portion.uk/assets/images/confetti.png'); background-size: cover; height: calc(100vh - 4em);">
+<div class="row masthead full-masthead main-masthead" style="overflow-x: hidden;background: linear-gradient(to top, rgba(256, 256, 256, 0.4), rgba(256, 256, 256, 0.1)), url('/wp-content/themes/portion.uk/assets/images/forest.png'); background-size: cover; height: calc(100vh - 4em);">
   <div class="container narrow">
     <div class="copy-block" data-jarallax-element="-30">
       <img class="logo" src="/wp-content/themes/portion.uk/assets/images/portion-logo-orange.png">
