d2263d631895e425a4815314f4adf63c848bbe5d
Beanies <beanies@Beaniess-iMac.local>
2019-07-27 14:27:03 +0100

    Green Logo

--- partials/main-masthead.php
+++ partials/main-masthead.php
@@ -1,7 +1,7 @@
 <div class="row masthead full-masthead main-masthead" style="overflow-x: hidden;background: linear-gradient(to top, rgba(256, 256, 256, 0.4), rgba(256, 256, 256, 0.1)), url('/wp-content/themes/portion.uk/assets/images/forest.png'); background-size: cover; height: calc(100vh - 4em);">
   <div class="container narrow">
     <div class="copy-block" data-jarallax-element="-30">
-      <img class="logo" src="/wp-content/themes/portion.uk/assets/images/portion-logo-orange.png">
+      <img class="logo" src="/wp-content/themes/portion.uk/assets/images/portion-logo-green.png">
       <h2>Give a portion of your Amazon order to charity with just one click.</h2>
       <a href="/"><button>Shop on Amazon<img src="/wp-content/themes/portion.uk/assets/images/arrow.png"></button></a>
     </div>
