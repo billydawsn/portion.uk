204a69f404f5ecd64ffde342335499ce9dac8a79
Beanies <beanies@Beaniess-iMac.local>
2019-07-28 16:31:42 +0100

    Analytics

--- header.php
+++ header.php
@@ -49,23 +49,15 @@
 
 
   <!-- Global site tag (gtag.js) - Google Analytics -->
-
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-129587290-1"></script>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-144714100-1"></script>
   <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'UA-129587290-1');
+    gtag('config', 'UA-144714100-1');
   </script>
 
-  <!-- Google Tag Manager -->
-  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-MSF4JFX');</script>
-  <!-- End Google Tag Manager -->
 
   <script src="https://code.jquery.com/jquery-latest.min.js"></script>
 
