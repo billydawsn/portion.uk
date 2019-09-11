d8e9437bfab1fc4216a9cbfa04165687135130de
Beanies <beanies@Beaniess-iMac.local>
2019-09-11 20:15:30 +0100

    Masthead change

--- partials/main-masthead.php
+++ partials/main-masthead.php
@@ -1,11 +1,15 @@
-<div class="row masthead full-masthead main-masthead" style="overflow-x: hidden;background: linear-gradient(to top, rgba(256, 256, 256, 0.4), rgba(256, 256, 256, 0.1)), url('/wp-content/themes/portion.uk/assets/images/forest.png'); background-size: cover; height: calc(100vh - 4em); background-position: 60% 0%;">
-  <div class="container narrow">
-    <div class="copy-block" data-jarallax-element="-30">
-      <img class="logo" src="/wp-content/themes/portion.uk/assets/images/portion-logo-green.png">
-      <h2>Give a portion of your Amazon order to charity with just one click.</h2>
-      <a href="https://www.amazon.co.uk?&_encoding=UTF8&tag=portion-21&linkCode=ur2&linkId=43594c8e104c82989c92adbeb17b5743&camp=1634&creative=6738"><button>Shop on Amazon<img src="/wp-content/themes/portion.uk/assets/images/arrow.png"></button></a>
-    </div>
-    <div class="header-scroll-icon"></div>
+<div class="row masthead full-masthead" style="margin-top: 10px;background: #FCFBF8; background-size: cover; height: calc(100vh - 20px);">
+
+  <div class="image_holder">
+    <div class="image left"><img src="assets/images/masthead_layer_0.png"></div>
+    <div class="image right"><img src="assets/images/masthead_layer_1.png"></div>
+    <div class="image right"><img src="assets/images/masthead_layer_2.png"></div>
+    <div class="image left"><img src="assets/images/masthead_layer_3.png"></div>
   </div>
-  <img class="hills" src="/wp-content/themes/portion.uk/assets/images/hills.svg">
+
+  <div class="container narrow">
+    <img class="logo" src="/wp-content/themes/portion.uk/assets/images/portion-logo-green.png">
+    <h2>Give a portion of your Amazon order to charity with just one click.</h2>
+    <a href="https://www.amazon.co.uk?&_encoding=UTF8&tag=portion-21&linkCode=ur2&linkId=43594c8e104c82989c92adbeb17b5743&camp=1634&creative=6738"><button>Shop on Amazon<img src="/wp-content/themes/portion.uk/assets/images/arrow.png"></button></a>
+  </div>
 </div>
